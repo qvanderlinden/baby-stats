ec410d645b3c37a38b0c8951893180e5b46e9d7a
Quentin Vanderlinden <quentin.vanderlinden@gmail.com>
2025-09-13 19:00:29 +0200

    feat: bottle model

--- backend/src/baby_stats/models.py
+++ backend/src/baby_stats/models.py
@@ -1,3 +1,17 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class DefaultModel(models.Model):
+    # id field is automatically added by Django
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True  # tells Django not to create a separate table for this
+
+
+class Bottle(DefaultModel):
+    datetime = models.DateTimeField()
+    quantity = models.IntegerField(validators=[MinValueValidator(0)])
+    burp = models.BooleanField()
