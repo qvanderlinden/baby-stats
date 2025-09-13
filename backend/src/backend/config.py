070e78e59f85dc750dda549aacb9bc5522a3fea7
Quentin Vanderlinden <quentin.vanderlinden@gmail.com>
2025-09-13 21:20:30 +0200

    chore: .env update

--- backend/src/backend/config.py
+++ backend/src/backend/config.py
@@ -13,15 +13,24 @@ class PostgresDatabase(BaseModel):
     password: str
 
 
+class DjangoConfig(BaseModel):
+    debug: bool = False
+    secret_key: str
+
+
 class Config(BaseSettings):
     model_config = SettingsConfigDict(
         env_file="../.env",
         env_file_encoding="utf-8",
         env_nested_delimiter="__",
-        extra="allow",
+        extra="ignore",
     )
 
     db: PostgresDatabase = Field(alias="POSTGRES_DB")
+    django: DjangoConfig = Field(alias="DJANGO")
 
 
 config = Config()
+
+if __name__ == "__main__":
+    print(config)
--- backend/src/mysite/settings.py
+++ backend/src/mysite/settings.py
@@ -20,11 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)e88ph=zjy3*ob%yzzu79q^r=(h=+2m1%s%b)^0pjv(7bt=#uh"
+SECRET_KEY = config.django.secret_key
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config.django.debug
 
 ALLOWED_HOSTS = []
 
