c094a637568f3e616b547b7d15fd82fccac0f3cb
Quentin Vanderlinden <quentin.vanderlinden@gmail.com>
2025-09-16 12:04:14 +0200

    feat: add api routes for bottles

--- backend/django_project/urls.py
+++ backend/django_project/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+
+from django_app_baby_stats import views
+
+router = routers.DefaultRouter()
+router.register(r"bottles", views.BottleViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("api/", include(router.urls)),
+    path("admin/", admin.site.urls),
 ]
