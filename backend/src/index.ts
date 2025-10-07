34c1222d2825b3e35af735f3d8c2504a1999e25b
Quentin Vanderlinden <quentin.vanderlinden@gmail.com>
2025-10-07 16:31:06 +0200

    feat: add better-auth handlers

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,11 @@
+import { auth } from "./auth";
+
+// TODO: use port from BACKEND__PORT
 const server = Bun.serve({
-  fetch(req) {
+  async fetch(req) {
+    const response = await auth.handler(req);
+    if (response) return response;
+
     return new Response("Not Found", { status: 404 });
   },
 });
